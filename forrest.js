78524c39b58b87a26040983606dcf7d9ac849792
Isaac Raymond <isaac.w.raymond@gmail.com>
2022-12-19 16:13:30 -0500

    test

---
+++ forrest.js
@@ -0,0 +1,8 @@
+//The sum of your player's health, attack, and defend
+//cannot be bigger than 50!
+
+var person1 = {name: "Johnny", heatlh: 10, attack: 10, defend: 10}
+var person2 = {name: "Ricky", heatlh: 10, attack: 10, defend: 10}
+var person3 = {name: "Susie", heatlh: 10, attack: 10, defend: 10}
+var person4 = {name: "Alice", heatlh: 10, attack: 10, defend: 10}
+var person5 = {name: "Dave", heatlh: 10, attack: 10, defend: 10}
